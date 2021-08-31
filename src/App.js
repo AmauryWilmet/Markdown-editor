1592f76992d144bbef3bef7ec47d79f5163d924a
AmauryWilmet <kaliroot08@gmail.com>
2021-08-31 10:01:30 +0200

    Complete markdown editor app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import ReactMarkdown from "react-markdown";
+import { Col, Container, Row } from "react-bootstrap";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [input, setInput] = useState();
+  const input2 = "# This is a header\n\nAnd this is a paragraph";
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+    console.log(input);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="container m-10">
+      <h1 className="text-center">Markdown-Editor</h1>
+      <Row>
+        <Col>
+          <textarea
+            className="border-4 border-blue-500 rounded"
+            rows="33"
+            cols="50"
+            onChange={handleChange}
+          ></textarea>
+        </Col>
+        <Col>
+          <ReactMarkdown>{input}</ReactMarkdown>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
